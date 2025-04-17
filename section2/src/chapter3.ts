22f48e92414e59301c7d93ba10cbb9041fbc01ec
ParkDH0809 <znxn1237@gmail.com>
2025-04-17 23:39:25 +0900

    feat: object, optional, readonly

---
+++ section2/src/chapter3.ts
@@ -0,0 +1,22 @@
+// object
+let user: {
+    id: number;
+    name: string;
+} = {
+    id: 1, 
+    name: "Typescript",
+};
+
+let dog: {
+    name: string,
+    color: string
+} = {
+    name: "강아지",
+    color: "brown",
+};
+
+let config: {
+    readonly apiKey: string;
+}= {
+    apiKey: "My Api Key",
+};
