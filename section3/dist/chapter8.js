4683d2667b5b2affd53091fb5feb11b2a9bc2cf5
ParkDH0809 <znxn1237@gmail.com>
2025-04-20 13:58:53 +0900

    feat: 서로소 유니온 타입

---
+++ section3/dist/chapter8.js
@@ -0,0 +1,48 @@
+function login(user) {
+    switch (user.tag) {
+        case "ADMIN": {
+            console.log(`${user.name}님 현재까지 ${user.kickCount}명 강퇴했습니다.`);
+            break;
+        }
+        case "MEMBER": {
+            console.log(`${user.name}님 현재까지 ${user.point}모았습니다`);
+            break;
+        }
+        case "GUEST": {
+            console.log(`${user.name}님 현재까지 ${user.visitCount}번 방문하셨습니다`);
+            break;
+        }
+    }
+}
+function processResult(task) {
+    switch (task.state) {
+        case "LOADING": {
+            console.log("로딩 중");
+            break;
+        }
+        case "FAILED": {
+            console.log(`에러 발생 : ${task.error.message}`);
+            break;
+        }
+        case "SUCCESS": {
+            console.log(`성공 : ${task.response.data}`);
+            break;
+        }
+    }
+}
+const loading = {
+    state: "LOADING",
+};
+const failed = {
+    state: "FAILED",
+    error: {
+        message: "오류 발생 원인은 ~~",
+    },
+};
+const success = {
+    state: "SUCCESS",
+    response: {
+        data: "데이터 ~~",
+    },
+};
+export {};
